26220d2c4db59c799f69e5f3b0e4df382698ecfb
reolch <reolch@github.com>
2024-11-20 13:01:33 +0900

    TODOを表示するところまで実装しました。

---
+++ TodoApp/TodoApp/ViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableview: UITableView!
+    
+    var todoList = [String]()
+    let userDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableview.dataSource = self
+        
+        // TODO: 保存されているToDoリストの読み込み
+    }
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        // TODO: TODOの追加機能を実装する
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // TODO: TODOの削除機能を実装する
+    }
+}
